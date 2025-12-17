d1c808df0ddf6775d9a88d28a9a0958276e6bde8
bullyto <apero.nuit.du.66@gmail.com>
2025-12-17 20:04:03 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
 // sw.js — FINAL FIX (ANTI-CHEAT SAFE)
 const CACHE_NAME = "hibou66-final-v1";
 
@@ -28,7 +30,7 @@ self.addEventListener("fetch", event => {
   const req = event.request;
 
   // 🚨 NEVER cache or intercept cheat image
-  if (req.url.includes("cheat.jpeg")) {
+  if (req.url.includes("tricherie.jpeg") || req.url.includes("cheat.jpeg")) {
     event.respondWith(fetch(req));
     return;
   }
